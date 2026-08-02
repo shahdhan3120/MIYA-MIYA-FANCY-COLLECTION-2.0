572b203bef397b60b48f591753049459f7e11216
shahdhan3120 <pvshahadhan@gmail.com>
2026-08-02 10:55:41 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,22 @@
-// Firebase SDK
+// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-// Firebase Config
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCE8aKjlr1PniIrcnN5-Pghmf0xoSZDhJQ",
   authDomain: "miya-miya-fancy.firebaseapp.com",
   projectId: "miya-miya-fancy",
   storageBucket: "miya-miya-fancy.firebasestorage.app",
   messagingSenderId: "135038886160",
-  appId: "1:135038886160:web:1293615012fb7070378595",
-  measurementId: "G-0T138X44MP"
+  appId: "1:135038886160:web:1293615012fb7070378595"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Firestore Database
+// Initialize Firestore
 const db = getFirestore(app);
 
-// Make database available to other scripts
-window.db = db;
-window.collection = collection;
-window.addDoc = addDoc;
-window.getDocs = getDocs;
-window.serverTimestamp = serverTimestamp;
+// Export Firestore
+export { db };
